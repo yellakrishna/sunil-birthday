a9f9337c24f9294b3042073d6a568d75a210d3aa
yellakrihsna <115071795+yellakrishna@users.noreply.github.com>
2023-12-21 14:51:28 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,40 @@
+
+
+function button(){
+   
+    let yella = document.getElementById("container");
+    yella.style.display="block"
+    
+  
+
+}
+
+function back(){
+   
+    let yella = document.getElementById("container");
+    yella.style.display="none"
+  
+
+}
+
+
+
+function spans(){
+    alert("HAPPY BIRTHDAY ..... [ MANGAL SUNIL KUMAR ]")
+}
+
+
+function congratulations(){
+    let pelliEl = document.getElementById("pelli");
+    pelliEl.style.display="block";
+    yella.style.display="none"
+
+}
+
+
+
+function pelli(){
+    let pelliEl = document.getElementById("pelli");
+    pelliEl.style.display="none";
+
+}
